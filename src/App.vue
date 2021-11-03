94db8bd75100b98664c0a2ed159944ff0bef82c7
DanielCheang007 <93252061+DanielCheang007@users.noreply.github.com>
2021-11-03 13:00:13 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Nav></Nav>
+  <div class="container">
+    <Login></Login>
+  </div>
+</template>
+
+<script>
+import Nav from "./components/Nav";
+import Login from "./components/Login";
+export default {
+  name: "App",
+  components: {
+    Nav,
+    Login,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
